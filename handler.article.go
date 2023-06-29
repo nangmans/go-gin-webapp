5d426253516ca7c81f40496f599d142cefb91c2f
nangmans <kallusis369@gmail.com>
2023-06-29 17:10:18 +0900

    Updating routing handler to respond with JSON/XML

--- handler.article.go
+++ handler.article.go
@@ -10,14 +10,10 @@ import (
 func showIndexPage(c *gin.Context) {
 	articles := getAllArticles()
 
-	c.HTML(
-		http.StatusOK,
-		"index.html",
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles,
+	}, "index.html")
 }
 
 func getArticle(c *gin.Context) {
@@ -36,5 +32,16 @@ func getArticle(c *gin.Context) {
 		}
 	} else {
 		c.AbortWithStatus(http.StatusNotFound)
+	}
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
 	}
 }
